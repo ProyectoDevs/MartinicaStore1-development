9fcdab7438195c2d0c43626e115de8e92e4c0f41
Jules9004 <julytroldan@yahoo.com>
2022-10-15 23:50:21 -0500

    Crud

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const router=express.Router();
 
-const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
 router.route('/productos').get(getProducts); //Establecemos desde qué ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct); //Establecemosla ruta
+router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+router.route('/producto/:id').put(updateProduct); //Creación de la ruta de la actualización
+//router.route('./producto:id').delete(deleteProduct); // eliminar el producto seleccionado
 
 module.exports = router;
