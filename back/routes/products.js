8181accbafd9ce5a6415991dde94143e7e92094e
Jules9004 <julytroldan@yahoo.com>
2022-11-05 20:22:44 -0500

    Reviews controller y route

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,7 +1,15 @@
 const express=require("express");
 const router=express.Router();
 
-const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const {getProducts, 
+    newProduct, 
+    getProductById, 
+    updateProduct, 
+    deleteProduct, 
+    createProductReview,
+    getProductReviews,
+    deleteReview
+} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route('/productos').get(getProducts); //Establecemos desde qué ruta queremos ver el getProducts
@@ -9,5 +17,9 @@ router.route('/producto/nuevo').post(isAuthenticatedUser, authorizeRoles("admin"
 router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
 router.route('/producto/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct); //Creación de la ruta de la actualización
 router.route('/producto/:id').delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct); //Creación de la ruta de eliminación
+
+router.route("/review").put(isAuthenticatedUser, createProductReview)
+router.route("/reviews").get(isAuthenticatedUser, getProductReviews)
+router.route("/review").delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
