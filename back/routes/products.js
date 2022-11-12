b2ad33beddb13888f8f75c74289ec4ed22e7cfaa
AlvaroG1016 <alvaro.g.b@outlook.com>
2022-11-11 22:12:32 -0500

    develop

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,25 +1,2 @@
-const express=require("express");
-const router=express.Router();
 
-const {getProducts, 
-    newProduct, 
-    getProductById, 
-    updateProduct, 
-    deleteProduct, 
-    createProductReview,
-    getProductReviews,
-    deleteReview
-} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
-
-router.route('/productos').get(getProducts); //Establecemos desde qué ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(isAuthenticatedUser, authorizeRoles("admin"), newProduct); //Establecemosla ruta
-router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
-router.route('/producto/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct); //Creación de la ruta de la actualización
-router.route('/producto/:id').delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct); //Creación de la ruta de eliminación
-
-router.route("/review").put(isAuthenticatedUser, createProductReview)
-router.route("/reviews").get(isAuthenticatedUser, getProductReviews)
-router.route("/review").delete(isAuthenticatedUser, deleteReview)
-
-module.exports = router;
+module.exports=router;
